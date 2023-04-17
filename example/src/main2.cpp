f4a1899a49036994dfd178adf5d49e8181d22c88
Salamence064 <thomas.ducote64@gmail.com>
2023-04-17 17:39:33 -0500

    Fixed memory leak I caused...

--- example/src/main2.cpp
+++ example/src/main2.cpp
@@ -113,9 +113,11 @@ void update() {
     PhysicsHandler::Object* objs = handler.getObject();
 
     cube1 = objs[0].collider.cube;
-    cube2 = objs[1].collider.cube;
+    cube2 = objs[1].collider.cube ;
     DrawRectPrism(&appState, cube1.getVertices(), {cube1.rb.pos.x, cube1.rb.pos.x, cube1.rb.pos.z}, {225.0 / 255.0, 1.0 / 255.0, 1.0, 1.0});
     DrawRectPrism(&appState, cube2.getVertices(), {cube2.rb.pos.x, cube2.rb.pos.x, cube2.rb.pos.z}, {225.0 / 255.0, 1.0 / 255.0, 1.0, 1.0});
+
+    delete[] objs;
 
     UpdateScene(&appState, fps.cam);
 
