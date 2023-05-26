89ca056d32ff618e63d151f16e93d74c5c1d8c7e
lennytanui <lennytanui@outlook.com>
2023-05-25 21:39:39 -0500

    build updates

--- demo/build.bat
+++ demo/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set ENV=MSVC
+set ENV=GNU
 
 if not exist ".\build" (
     echo "Creating `.\build` directory"
@@ -12,6 +12,11 @@ if not exist ".\build\vendor" (
     mkdir ".\build\vendor"
 )
 
+if not exist "..\libs" (
+    echo "Creating `.\libs` directory"
+    mkdir "..\libs"
+)
+
 WHERE em++ /Q
 if ERRORLEVEL 1 (
     call "E:\dev\learn_emsc\emsdk\emsdk_env.bat"
@@ -20,70 +25,62 @@ if ERRORLEVEL 1 (
 if exist ".\vendor\glfw3.dll"( GOTO skip_GLFW_build )
 if %ENV% == WEB ( GOTO skip_GLFW_build)
 
-if not exist ".\vendor\glfw3.dll" ( 
-    echo Building GLFW
-    copy ".\glfw_config.h" "..\glfw-3.3.8\src" >nul
-    pushd "../glfw-3.3.8/src"
+echo Building GLFW
+copy ".\glfw_config.h" "..\glfw-3.3.8\src" >nul
+pushd "../glfw-3.3.8/src"
 
-    if not exist "..\..\include\GLFW" (
-        robocopy ..\include\GLFW\ ..\..\include\GLFW  /E /NJH /NJS /NFL /NDL
-    )
-
-    if not %ENV% == WEB (
-
-        if not exist ".\build" (
-            echo "Creating `.\build` directory"
-            mkdir ".\build\"
-        )
-
-        if not exist ".\temp" (
-            echo "Creating `.\build` directory"
-            mkdir ".\temp\"
-        )  
-
-        @REM move files not related to windows to temporary directory
-        set source_folder=.\
-        set search_words=x11 wl linux cocoa glx xkb posix null
-        set destination_folder=.\temp
-
-        for %%W in (%search_words%) do (
-            for /R %source_folder% %%F in (*%%W*) do (
-                move "%%F" "%destination_folder%" >nul
-            )
-        )
-
-        pushd "./build"
-            @REM cl /D_USRDLL /D_WINDLL -I../ -I../../deps ../*.c -D_GLFW_USE_CONFIG_H -D_GLFW_BUILD_DLL Gdi32.lib User32.lib Shell32.lib /link /DLL /OUT:glfw3.dll
-            gcc -c ../*.c -I../../deps -D_GLFW_BUILD_DLL -D_GLFW_USE_CONFIG_H -lOpengl32 -lShell32 -lGdi32 -lUser32
-            gcc -shared -o glfw3.dll *.o -Wl,--out-implib,libglfw3.a -lOpengl32 -lShell32 -lGdi32 -lUser32
-            copy "glfw3.dll" "../../../demo/vendor" 
-            copy "libglfw3.a" "../../../libs"
-        popd
-
-        @REM restore files from temporary directory
-        for %%W in (%search_words%) do (
-            for /R %destination_folder% %%F in (*%%W*) do (
-                move "%%F" "%source_folder%" >nul
-            )
-        )
-
-        set source_folder = 
-        set destination_folder = 
-        set search_words = 
-    )
-
-    popd
-
-
+if not exist "..\..\include\GLFW" (
+    robocopy ..\include\GLFW\ ..\..\include\GLFW  /E /NJH /NJS /NFL /NDL
 )
 
+if not exist ".\build" (
+    echo "Creating `.\build` directory"
+    mkdir ".\build\"
+)
+
+if not exist ".\temp" (
+    echo "Creating `.\build` directory"
+    mkdir ".\temp\"
+)  
+
+@REM move files not related to windows to temporary directory
+set source_folder=.\
+set search_words=x11 wl linux cocoa glx xkb posix null
+set destination_folder=.\temp
+
+for %%W in (%search_words%) do (
+    for /R %source_folder% %%F in (*%%W*) do (
+        move "%%F" "%destination_folder%" >nul
+    )
+)
+
+pushd "./build"
+    @REM cl /D_USRDLL /D_WINDLL -I../ -I../../deps ../*.c -D_GLFW_USE_CONFIG_H -D_GLFW_BUILD_DLL Gdi32.lib User32.lib Shell32.lib /link /DLL /OUT:glfw3.dll
+    gcc -c ../*.c -I../../deps -D_GLFW_BUILD_DLL -D_GLFW_USE_CONFIG_H -lOpengl32 -lShell32 -lGdi32 -lUser32
+    gcc -shared -o glfw3.dll *.o -Wl,--out-implib,libglfw3.a -lOpengl32 -lShell32 -lGdi32 -lUser32
+    copy "glfw3.dll" "../../../demo/vendor" 
+    copy "libglfw3.a" "../../../libs"
+popd
+
+@REM restore files from temporary directory
+for %%W in (%search_words%) do (
+    for /R %destination_folder% %%F in (*%%W*) do (
+        move "%%F" "%source_folder%" >nul
+    )
+)
+
+set source_folder = 
+set destination_folder = 
+set search_words = 
+
+popd
 
 :skip_GLFW_build
 pushd "./build"
 
 if %ENV% == GNU (
     @REM Compiling with g++
-    g++ -g -std=c++14 ..\main.cpp ..\app.cpp -o app -I..\..\include -L..\..\libs -l:libglfw3_lib.a -lOpengl32
+    g++ -g -std=c++14 ..\main.cpp ..\app.cpp -o app -I..\..\include -L..\..\libs -l:libglfw3.a -lOpengl32
 )
 
 if %ENV% == MSVC (
